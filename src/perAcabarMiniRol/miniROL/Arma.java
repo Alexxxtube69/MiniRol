de8f407cf1e2b132e01494046fcdc1aae93f153b
Alexxx <115105013+Alexxxtube69@users.noreply.github.com>
2025-11-07 16:15:27 +0100

    toques finales

--- src/perAcabarMiniRol/miniROL/Arma.java
+++ src/perAcabarMiniRol/miniROL/Arma.java
@@ -1,9 +1,10 @@
 package perAcabarMiniRol.miniROL;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Random;
 
-public class Arma {
+public class Arma  implements Serializable {
 
     private String nom;
     private String descripcio;
--- src/perAcabarMiniRol/miniROL/FinestraPrincipal.java
+++ src/perAcabarMiniRol/miniROL/FinestraPrincipal.java
@@ -2,6 +2,7 @@ package perAcabarMiniRol.miniROL;
 
 import javax.swing.*;
 import java.awt.*;
+import java.io.*;
 
 import marcPanellPersonalitzat.FramePer;
 import perAcabarMiniRol.miniROL.Botiga;
@@ -16,6 +17,7 @@ public class FinestraPrincipal {
     private JLabel etImatge;
 
     private JButton botExplorar, botBotiga, botoTriaClasse;
+    private JButton botGuardaInfo, botMostraInfo;
 
     private Personatge pj;
 
@@ -39,6 +41,8 @@ public class FinestraPrincipal {
         botExplorar = new JButton("Explorar");
         botBotiga = new JButton("Botiga");
         botoTriaClasse = new JButton("Tria Classe");
+        botGuardaInfo = new JButton("Guardar");
+        botMostraInfo = new JButton("Carraga");
 
     }
 
@@ -75,9 +79,16 @@ public class FinestraPrincipal {
         botExplorar.addActionListener(e->novaExploracio());
         botBotiga.addActionListener(e -> obrirBotiga());
         botoTriaClasse.addActionListener(e -> triaClasse());
+
+        botGuardaInfo.addActionListener(e -> serializaPersonatge(pj));
+        botMostraInfo.addActionListener(e -> mostraInfoPersonatge());
+
         panellInferior.add(botExplorar);
         panellInferior.add(botBotiga);
         panellInferior.add(botoTriaClasse);
+
+        panellInferior.add(botMostraInfo);
+        panellInferior.add(botGuardaInfo);
 
         panellPrincipal.add(panellSuperior, BorderLayout.NORTH);
         panellPrincipal.add(panellInferior, BorderLayout.SOUTH);
@@ -90,6 +101,45 @@ public class FinestraPrincipal {
 
 
     }
+
+    private static void serializaPersonatge(Personatge pj) {
+        String nomFitxer = JOptionPane.showInputDialog("Escriu el nom del fitxe: ");
+        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(nomFitxer))) {
+            oos.writeObject(pj);
+            JOptionPane.showMessageDialog(null, "sa guardad la informcaio del personatge");
+            System.out.println(nomFitxer);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    //void treura string
+    private void mostraInfoPersonatge() {
+        String nomFitxer = JOptionPane.showInputDialog("Escriu el nom del fitxe que vols recupera: ");
+        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(nomFitxer))) {
+
+            Personatge carrega = (Personatge) ois.readObject();
+            JOptionPane.showMessageDialog(null, "Personatge carregat correctament");
+
+            this.pj = carrega;
+
+            etNom.setText(pj.getNom() + "       ");
+            etNivell.setText(" Lvl:" + pj.getNivell());
+            etXp.setText(" Xp:" + pj.getXp() + "/" + pj.getXpNecessaria());
+            etOr.setText(" Or: " + pj.getOr());
+            etAtributs.setText(" Atc:" + pj.getAtac() + " | Def: " + pj.getDefensa() + " | Agl: " + pj.getAgilitat());
+
+            panellSuperior.revalidate();
+            panellSuperior.repaint();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+    }
+
 
     private void equiparArma() {
 
